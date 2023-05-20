94ea01dc3adb217e24e0f7e962d942d73a69dc91
«ChulakovEvgeniy» <>
2023-05-20 21:02:34 +0300

    поправлена 8 задача

--- task8.py
+++ task8.py
@@ -9,7 +9,8 @@ rows = int(input('Введите количество полосок шокол�
 columns = int(input('Введите кол-во столбиков шоколада: '))
 need = int(input('Введите кол-во ячеек шоколада, которые вы хотите отломить:'))
 
-if need % rows == 0 and need <= rows * columns or need % columns == 0 and need <= rows * columns:
-    print('Можно')
+if need <= rows * columns:
+    if need % rows == 0 or need % columns == 0:
+        print('Можно')
 else:
     print('Нельзя')
